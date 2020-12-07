a6c790707f67003bf26370877a5feb607d1a4497
Dan Hicks <hicks.daniel.j@gmail.com>
2020-12-07 12:56:19 -0800

    match _deploy.sh in lecture notes

--- _deploy.sh
+++ _deploy.sh
@@ -5,12 +5,12 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "hicks.daniel.j@gmail.com"
-git config --global user.name "Dan Hicks"
+git config --global user.email "your@email.here"
+git config --global user.name "Your Name"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git temp
 cd temp
-mv -f ../scripts/run_metas.html index.html
+mv -f ../scripts/OUTPUTFILE.html index.html
 git add index.html
 git commit -m "Update the reproduced analysis" || true
 git push origin gh-pages
